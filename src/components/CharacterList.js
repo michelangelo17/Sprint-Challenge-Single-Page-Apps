c9edc98be3553b700eed02974273e8d9c5ef15ba
michelangelo17 <michelangelo.markus@gmail.com>
2019-11-15 09:01:32 -0800

    api call for characters working

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,13 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import CharactersAPI from "../apis/CharactersAPI";
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
-
-  // useEffect(() => {
-  //   // TODO: Add API Request here - must run in `useEffect`
-  //   //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  // }, []);
-
+  const charArray = CharactersAPI()
   return (
     <section className="character-list">
       <h2>TODO: `array.map()` over your state here!</h2>
